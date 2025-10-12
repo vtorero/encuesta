789b2e9439776d1dc2a19ea8db27339c96393a31
Victor J <vtorero@gmail.com>
2025-10-12 17:19:14 -0500

    add

--- src/app/encuesta/encuesta.component.ts
+++ src/app/encuesta/encuesta.component.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA, MatDialog } from '@angular/material/dialog';
-import { FormBuilder, FormGroup, Validators,ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators,ReactiveFormsModule, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'encuesta',
@@ -12,11 +12,17 @@ export class EncuestaComponent implements OnInit {
   form!: FormGroup;
 
   constructor(private fb: FormBuilder) {}
-
+   get Dependientes() {
+      return this.form.get('dependientes') as FormArray;
+    }
   ngOnInit(): void {
     this.form = this.fb.group({
       shortText: ['', Validators.required],
-    
+      dependientes: this.fb.array([this.fb.group({
+      nombres: ['', Validators.required],
+      parentesco:['',Validators.required],
+      edad: [0, [Validators.required, Validators.min(0)]]
+    })]),
       address: [''],
       city: [''],
       country: [''],
@@ -36,6 +42,18 @@ export class EncuestaComponent implements OnInit {
       this.form.markAllAsTouched();
     }
   }
+onAddToPago():void{
+  const PagoGroup = this.fb.group({
+		nombres: ['', Validators.required],
+    parentesco:['',Validators.required],
+    edad: ['', [Validators.required, Validators.min(0)]],
+	});
+	this.Dependientes.push(PagoGroup);
+}
+
+onDeletePago(index:number):void{
+  this.Dependientes.removeAt(index);
+}
 
   onReset(): void {
     this.form.reset({
